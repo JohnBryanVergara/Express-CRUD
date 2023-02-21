4122ccbd954063d05f49c7fd444a5f788450ca96
JohnBryanVergara <vergarajohnbryan@gmail.com>
2023-02-21 22:22:15 +0800

    Version : update 10:22PM

--- index.ts
+++ index.ts
@@ -5,26 +5,58 @@ const app = express();
 const prisma = new PrismaClient();
 const port = 3000;
 
+//Middleware
 app.use(express.json());
 
+//POST - CREATE
 app.post("/register", async (req, res) => {
-    // const name = req.body.name;
-    // const email = req.body.email;
-    const { name, email } = req.body;
+    const { id, title, content, published } = req.body;
 
-    const user = await prisma.user.create({
-        data: {
-            name,
-            email,
-        },
+    const post = await prisma.post.create({
+        data: [
+            {
+                id,
+                title,
+                content,
+                published: false,
+            },
+        ],
     });
 
-    res.json({ user });
+    res.json({ post });
 });
 
-app.get("/users", async (req, res) => {
-    const users = await prisma.user.findMany();
-    res.json({ users });
+//GET - READ
+app.get("/material", async (req, res) => {
+    const materials = await prisma.post.findMany();
+    res.json({ materials });
+});
+
+//PUT - UPDATE
+app.put("/update/:id", async (req, res) => {
+    const { id, title } = req.body;
+
+    const updatePosts = await prisma.post.update({
+        where: {
+            id,
+        },
+        data: {
+            title,
+        },
+    });
+    res.json({ updatePosts });
+});
+
+//DELETE - DELETE
+app.delete("/delete/:id", async (req, res) => {
+    const { id } = req.body;
+
+    const delUser = await prisma.post.delete({
+        where: {
+            id,
+        },
+    });
+    res.json({ delUser });
 });
 
 app.listen(port, () => {
